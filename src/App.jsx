22ea78e23fa524e55bafeaa61d4b04482eff52c3
Nguyễn Duy Duẩn <94855872+nguyenduyduan@users.noreply.github.com>
2023-10-05 10:55:18 +0700

    set up for rendering page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar.jsx'
+import './index.css'
+import Carousel from './components/Carousel.jsx'
+import Card from './components/Card'
+
+const logoUrl = '/assets/test.png';
+const logo = 'TEAM 3';
+const pages = [
+  { name: 'Home', url: '/' },
+  { name: 'About Us', url: '/about-us' },
+  { name: 'Products', url: '/all-product'},
+  { name: 'Blog', url: '/blog'},
+  { name: 'Contact Us', url: '/contact-us'},
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar logoUrl={reactLogo} logo = {logo} pages={pages} />
+      <Card/>
+      <Carousel/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import NavBar from './components/NavBar.jsx'
-import './index.css'
-
-const logoUrl = '/assets/test.png';
-const logo = 'TEAM 3';
-const pages = [
-  { name: 'Home', url: '/' },
-  { name: 'About Us', url: '/about-us' },
-  { name: 'Products', url: '/all-product'},
-  { name: 'Blog', url: '/blog'},
-  { name: 'Contact Us', url: '/contact-us'},
-];
+import ErrorPage from './components/ErrorPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NavBar logoUrl={logoUrl} logo = {logo} pages={pages} />
+    <App/>
+    <ErrorPage/>
   </React.StrictMode>,
 )
