386bab7f39573a84f605b261e5c1302682a3437e
mifox22 <147582445+mifox22@users.noreply.github.com>
2023-11-30 11:42:30 +0700

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,14 +9,17 @@ import ProductDetail from './components/ProductDetail'
 import Cart from './components/Cart'
 import Banner from './components/Banner'
 import ErrorPage from './components/ErrorPage'
+import ProductsGrid from './components/ProductsGrid.jsx'
 
 const logo = 'TEAM 3';
 const pages = [
-  { name: 'Home', url: '/' },
-  { name: 'About Us', url: '/about-us' },
-  { name: 'Products', url: '/all-product'},
-  { name: 'Blog', url: '/blog'},
-  { name: 'Contact Us', url: '/contact-us'},
+ { name: 'Trang Chủ', url: '/',dt:[] },
+  { name: 'Có Gì Mới', url: '/about-us' ,dt:['Bản tin','Blog']},
+   { name: 'Thương Hiệu', url: '/all-product',dt:['Bản tin','Blog']},
+   { name: 'Sản Phẩm', url: '/blog',dt:['Bản tin','Blog']},
+   { name: 'Sale Off', url: '#',dt:[]},
+   { name: 'Blog', url: '#',dt:['Bản tin','Blog']},
+   { name: 'Về Gakki', url: '#',dt:['Bản tin','Blog']},
 ];
 
 function App() {
@@ -25,6 +28,7 @@ function App() {
       <NavBar logoUrl={reactLogo} logo = {logo} pages={pages} />
       <ErrorPage></ErrorPage>
       <img src="https://vn.yamaha.com/vi/files/Brand_Image_Drums_pc_vn_f502df3148e42fb97023a31588e1d127.jpg?impolicy=resize&imwid=2000&imhei=800" alt="Background Test"/>
+      <ProductsGrid/>
       <Card/>
       {/* <ProductDetail/> */}
       <Cart/>
