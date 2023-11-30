02b57c37fd2a24894f4b30196964218c23399b81
mifox22 <147582445+mifox22@users.noreply.github.com>
2023-11-30 11:40:06 +0700

    Create Card2.jsx

---
+++ Frontend/src/components/Card2.jsx
@@ -0,0 +1,264 @@
+import React from "react";
+
+function Card2() {
+  return (
+    <div className="w-100 px-12">
+      <div className="grid grid-cols-1 3xl:grid-cols-3 md:grid-cols-4 gap-4 p-4 px-4">
+        <div className="bg-white shadow-lg rounded-lg max-w-sm">
+          <div class="relative overflow-hidden">
+            <a>
+              <img
+                className="rounded-t-lg p-8 object-cover"
+                src="https://i.ibb.co/KqdgGY4/cosmetic-packaging-mockup-1150-40280.webp"
+                alt="product image"
+              />
+            </a>
+            <div class="absolute h-full w-full bg-green-100/20 flex items-center justify-center -bottom-10 hover:bottom-0 opacity-0 hover:opacity-100 transition-all duration-300">
+              <button class="bg-green-400 text-white py-3 px-6 opacity-60 hover:opacity-100 font-semibold rounded ">
+                Xem nhanh
+              </button>
+            </div>
+          </div>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h3 className="text-gray-900 font-semibold text-center text-xl tracking-tight">
+                Guitar martin gỗ little LX1E
+              </h3>
+            </a>
+            <div className="flex items-center  justify-center mt-2.5 mb-5">
+              <h5 className="text-gray-900 text-xl tracking-tight">
+                1.000.000 VNĐ
+              </h5>
+            </div>
+            <div className="flex items-center justify-center">
+              <a
+                href="#"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                ĐẶT MUA <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white shadow-lg rounded-lg max-w-sm">
+          <div class="relative overflow-hidden">
+            <a>
+              <img
+                className="rounded-t-lg p-8 object-cover"
+                src="https://i.ibb.co/KqdgGY4/cosmetic-packaging-mockup-1150-40280.webp"
+                alt="product image"
+              />
+            </a>
+            <div class="absolute h-full w-full bg-green-100/20 flex items-center justify-center -bottom-10 hover:bottom-0 opacity-0 hover:opacity-100 transition-all duration-300">
+              <button class="bg-green-400 text-white py-3 px-6 opacity-60 hover:opacity-100 font-semibold rounded ">
+                Xem nhanh
+              </button>
+            </div>
+          </div>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h3 className="text-gray-900 font-semibold text-center text-xl tracking-tight">
+                Guitar martin gỗ little LX1E
+              </h3>
+            </a>
+            <div className="flex items-center  justify-center mt-2.5 mb-5">
+              <h5 className="text-gray-900 text-xl tracking-tight">
+                1.000.000 VNĐ
+              </h5>
+            </div>
+            <div className="flex items-center justify-center">
+              <a
+                href="#"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                ĐẶT MUA <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white shadow-lg rounded-lg max-w-sm">
+          <div class="relative overflow-hidden">
+            <a>
+              <img
+                className="rounded-t-lg p-8 object-cover"
+                src="https://i.ibb.co/KqdgGY4/cosmetic-packaging-mockup-1150-40280.webp"
+                alt="product image"
+              />
+            </a>
+            <div class="absolute h-full w-full bg-green-100/20 flex items-center justify-center -bottom-10 hover:bottom-0 opacity-0 hover:opacity-100 transition-all duration-300">
+              <button class="bg-green-400 text-white py-3 px-6 opacity-60 hover:opacity-100 font-semibold rounded ">
+                Xem nhanh
+              </button>
+            </div>
+          </div>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h3 className="text-gray-900 font-semibold text-center text-xl tracking-tight">
+                Guitar martin gỗ little LX1E
+              </h3>
+            </a>
+            <div className="flex items-center  justify-center mt-2.5 mb-5">
+              <h5 className="text-gray-900 text-xl tracking-tight">
+                1.000.000 VNĐ
+              </h5>
+            </div>
+            <div className="flex items-center justify-center">
+              <a
+                href="#"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                ĐẶT MUA <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white shadow-lg rounded-lg max-w-sm">
+          <div class="relative overflow-hidden">
+            <a>
+              <img
+                className="rounded-t-lg p-8 object-cover"
+                src="https://i.ibb.co/KqdgGY4/cosmetic-packaging-mockup-1150-40280.webp"
+                alt="product image"
+              />
+            </a>
+            <div class="absolute h-full w-full bg-green-100/20 flex items-center justify-center -bottom-10 hover:bottom-0 opacity-0 hover:opacity-100 transition-all duration-300">
+              <button class="bg-green-400 text-white py-3 px-6 opacity-60 hover:opacity-100 font-semibold rounded ">
+                Xem nhanh
+              </button>
+            </div>
+          </div>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h3 className="text-gray-900 font-semibold text-center text-xl tracking-tight">
+                Guitar martin gỗ little LX1E
+              </h3>
+            </a>
+            <div className="flex items-center  justify-center mt-2.5 mb-5">
+              <h5 className="text-gray-900 text-xl tracking-tight">
+                1.000.000 VNĐ
+              </h5>
+            </div>
+            <div className="flex items-center justify-center">
+              <a
+                href="#"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                ĐẶT MUA <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white shadow-lg rounded-lg max-w-sm">
+          <div class="relative overflow-hidden">
+            <a>
+              <img
+                className="rounded-t-lg p-8 object-cover"
+                src="https://i.ibb.co/KqdgGY4/cosmetic-packaging-mockup-1150-40280.webp"
+                alt="product image"
+              />
+            </a>
+            <div class="absolute h-full w-full bg-green-100/20 flex items-center justify-center -bottom-10 hover:bottom-0 opacity-0 hover:opacity-100 transition-all duration-300">
+              <button class="bg-green-400 text-white py-3 px-6 opacity-60 hover:opacity-100 font-semibold rounded ">
+                Xem nhanh
+              </button>
+            </div>
+          </div>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h3 className="text-gray-900 font-semibold text-center text-xl tracking-tight">
+                Guitar martin gỗ little LX1E
+              </h3>
+            </a>
+            <div className="flex items-center  justify-center mt-2.5 mb-5">
+              <h5 className="text-gray-900 text-xl tracking-tight">
+                1.000.000 VNĐ
+              </h5>
+            </div>
+            <div className="flex items-center justify-center">
+              <a
+                href="#"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                ĐẶT MUA <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white shadow-lg rounded-lg max-w-sm">
+          <div class="relative overflow-hidden">
+            <a>
+              <img
+                className="rounded-t-lg p-8 object-cover"
+                src="https://i.ibb.co/KqdgGY4/cosmetic-packaging-mockup-1150-40280.webp"
+                alt="product image"
+              />
+            </a>
+            <div class="absolute h-full w-full bg-green-100/20 flex items-center justify-center -bottom-10 hover:bottom-0 opacity-0 hover:opacity-100 transition-all duration-300">
+              <button class="bg-green-400 text-white py-3 px-6 opacity-60 hover:opacity-100 font-semibold rounded ">
+                Xem nhanh
+              </button>
+            </div>
+          </div>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h3 className="text-gray-900 font-semibold text-center text-xl tracking-tight">
+                Guitar martin gỗ little LX1E
+              </h3>
+            </a>
+            <div className="flex items-center  justify-center mt-2.5 mb-5">
+              <h5 className="text-gray-900 text-xl tracking-tight">
+                1.000.000 VNĐ
+              </h5>
+            </div>
+            <div className="flex items-center justify-center">
+              <a
+                href="#"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                ĐẶT MUA <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white shadow-lg rounded-lg max-w-sm">
+          <div class="relative overflow-hidden">
+            <a>
+              <img
+                className="rounded-t-lg p-8 object-cover"
+                src="https://i.ibb.co/KqdgGY4/cosmetic-packaging-mockup-1150-40280.webp"
+                alt="product image"
+              />
+            </a>
+            <div class="absolute h-full w-full bg-green-100/20 flex items-center justify-center -bottom-10 hover:bottom-0 opacity-0 hover:opacity-100 transition-all duration-300">
+              <button class="bg-green-400 text-white py-3 px-6 opacity-60 hover:opacity-100 font-semibold rounded ">
+                Xem nhanh
+              </button>
+            </div>
+          </div>
+          <div className="px-5 pb-5">
+            <a href="#">
+              <h3 className="text-gray-900 font-semibold text-center text-xl tracking-tight">
+                Guitar martin gỗ little LX1E
+              </h3>
+            </a>
+            <div className="flex items-center  justify-center mt-2.5 mb-5">
+              <h5 className="text-gray-900 text-xl tracking-tight">
+                1.000.000 VNĐ
+              </h5>
+            </div>
+            <div className="flex items-center justify-center">
+              <a
+                href="#"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                ĐẶT MUA <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card2;
