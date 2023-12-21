fc1fc375b5cc9ae07f8a90ead9a616e5e6df9015
Brian09102003 <vonhan09102003@gmail.com>
2023-12-22 04:55:15 +0700

    AdminOrder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,20 +7,13 @@ import Register from './pages/Register'
 import ErrorPage from './pages/ErrorPage'
 import Home from './pages/Home'
 import Admin from './pages/Admin'
+import AdminOrder from './components/AdminOrder'
 
 
 function App() {
   // const user = useSelector(state => state.user)
   return (
-    <>
-      <Routes className = 'root font-nunito '>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='*' element={<ErrorPage/>} />
-        <Route path='/admin' element={<Admin/>} />
-      </Routes>
-    </>
+    <AdminOrder/>
   )
 }
 
