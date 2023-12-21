9e890cc8412464dd34950fd2ae7e674c142b93cb
Thao Nguyen <nguyennt21411@st.uel.edu.vn>
2023-12-21 18:49:19 +0700

    cart

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,23 @@
-import reactLogo from './assets/react.svg'
-import './App.css'
-import './index.css'
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import "./index.css";
 
-import NavBar from './components/NavBar.jsx'
-import Carousel from './components/Carousel.jsx'
-import Card from './components/Card'
-import ProductDetail from './components/ProductDetail.jsx'
-import Cart from './components/Cart'
-import Banner from './components/Banner'
-import ErrorPage from './components/ErrorPage'
-import AboutUs from './components/AboutUs.jsx'
-
-
+import NavBar from "./components/NavBar.jsx";
+import Carousel from "./components/Carousel.jsx";
+import Card from "./components/Card";
+import ProductDetail from "./components/ProductDetail.jsx";
+import Cart from "./components/Cart";
+import Banner from "./components/Banner";
+import ErrorPage from "./components/ErrorPage";
+import AboutUs from "./components/AboutUs.jsx";
 
 function App() {
   return (
     <>
-      <AboutUs></AboutUs>
-      
+      <NavBar></NavBar>
+      <Cart></Cart>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
