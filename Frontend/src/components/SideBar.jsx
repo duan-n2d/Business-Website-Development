f98c3050035fa9055eb704dee38111b23592bc60
Nguyễn Duy Duẩn <94855872+nguyenduyduan@users.noreply.github.com>
2023-12-28 15:11:00 +0700

    complete SideBar and Admin but not complete Admin Content

--- Frontend/src/components/SideBar.jsx
+++ Frontend/src/components/SideBar.jsx
@@ -8,7 +8,11 @@ import { PiGuitarFill } from "react-icons/pi";
 import { PiNotePencilBold } from "react-icons/pi";
 import { PiSignOutBold } from "react-icons/pi";
 
-const Sidebar = () => {
+const Sidebar = ({ setSelectedComponent }) => {
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+    }
   return (
     <div className="relative">
         <aside id="sidebar-multi-level-sidebar" className="w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
@@ -20,55 +24,62 @@ const Sidebar = () => {
             <li>
                 <button
                 type="button"
+                onClick={() => setSelectedComponent("dashboard")}
                 className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                     <PiChartPieSliceFill className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true"/>
                     <span className="ms-3">Dashboard</span>
                 </button>
             </li>
             <li>
-            <button
-            type="button"
-            className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
-            <PiBasketFill className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" />
-            <span className="ms-3 text-left rtl:text-right whitespace-nowrap">Đơn hàng</span>
-          </button>
+                <button
+                    type="button"
+                    onClick={() => setSelectedComponent("order")}
+                    className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                    <PiBasketFill className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" />
+                    <span className="ms-3 text-left rtl:text-right whitespace-nowrap">Đơn hàng</span>
+                </button>
             </li>
             <li>
                 <button
-                type="button"
-            className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                    type="button"
+                    onClick={() => setSelectedComponent("customer")}
+                    className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                     <PiUsersBold className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true"/>
                     <span className="ms-3 whitespace-nowrap">Khách hàng</span>
                 </button>
             </li>
             <li>
                 <button
-                type="button"
-            className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                    type="button"
+                    onClick={() => setSelectedComponent("blog")}
+                    className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                     <PiNotePencilBold className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true"/>
                     <span className="ms-3 whitespace-nowrap">Bài viết</span>
                 </button>
             </li>
             <li>
                 <button
-                type="button"
-            className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                    type="button"
+                    onClick={() => setSelectedComponent("product")}
+                    className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                     <PiGuitarFill className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true"/>
                     <span className="ms-3 whitespace-nowrap">Sản phẩm</span>
                 </button>
             </li>
             <li>
                 <button
-                type="button"
-            className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                    type="button"
+                    onClick={() => setSelectedComponent("delivery")}
+                    className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                     <PiTruckFill className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true"/>
                     <span className="ms-3 whitespace-nowrap">Vận chuyển</span>
                 </button>
             </li>
             <li>
                 <button
-                type="button"
-            className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                    type="button"
+                    onClick={handleLogout}
+                    className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                     <PiSignOutBold className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"/>
                     <span className="ms-3 whitespace-nowrap">Đăng xuất</span>
                 </button>
--- Frontend/src/pages/Admin.jsx
+++ Frontend/src/pages/Admin.jsx
@@ -1,13 +1,52 @@
-import React from "react";
-
+import React, { useState } from "react";
 import Header from "../components/Header";
 import Sidebar from "../components/SideBar";
+import Sales from "../components/Sales";
+import AdminBlog from "../components/AdminBlog";
+import AdminProduct from "../components/AdminProducts";
+import AdminOrder from "../components/AdminOrder";
+import AdminCustomer from "../components/AdminCustomer";
 
 export default function Admin() {
-    return (
-        <div>
-            <Header />
-            <Sidebar />
+    const [selectedComponent, setSelectedComponent] = useState("dashboard");
+  
+    const scrollToTop = () => {
+      window.scrollTo(0, 0);
+    }; 
+
+    const renderComponent = () => {
+    switch (selectedComponent) {
+      case "dashboard":
+        return <Sales />;
+      case "order":
+        return <AdminOrder />;
+      case "customer":
+        return <AdminCustomer />;
+      case "blog":
+        return <AdminBlog />;
+      case "product":
+        return <AdminProduct />;
+      default:
+        return null;
+    }
+  };
+
+  const handleComponentChange = (component) => {
+    setSelectedComponent(component);
+    scrollToTop();
+  };
+
+  return (
+    <div className="flex">
+      <div className="fixed">
+        <Sidebar setSelectedComponent={handleComponentChange} />
+      </div>
+      <div className="w-full bg-green-50">
+        <div className="fixed z-50">
+          <Header />
         </div>
-    );
+        <div className="content mt-24 ml-64 p-10">{renderComponent()}</div>
+      </div>
+    </div>
+  );
 }
