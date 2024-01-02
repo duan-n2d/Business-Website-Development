a6b4c5bfe3ab22f9dc9cd354dc120cfdec7bc7dd
Brian09102003 <vonhan09102003@gmail.com>
2024-01-02 18:23:59 +0700

    Brand
    Đã có hình

--- Frontend/src/components/Brand.jsx
+++ Frontend/src/components/Brand.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { GiGuitarBassHead, GiJackPlug } from "react-icons/gi";
+import { CgPiano } from "react-icons/cg";
 
 function Brand() {
     return (
@@ -10,29 +12,30 @@ function Brand() {
                 </div>
             </div>
 
-            <div className="grid grid-flow-row lg:grid-cols-8 md:grid-cols-4 grid-cols-2 pt-5">
-                <div><a href="#"><img src="./src/assets/Gakki.png" alt="Gakki" className=" lg:pl-0 lg:pr-0 pl-5 pr-5" /></a></div>
-                <div><a href="#"><img src="./src/assets/Gakki.png" alt="Gakki" className=" lg:pl-0 lg:pr-0 pl-5 pr-5" /></a></div>
-                <div><a href="#"><img src="./src/assets/Gakki.png" alt="Gakki" className=" lg:pl-0 lg:pr-0 pl-5 pr-5" /></a></div>
-                <div><a href="#"><img src="./src/assets/Gakki.png" alt="Gakki" className=" lg:pl-0 lg:pr-0 pl-5 pr-5" /></a></div>
-                <div><a href="#"><img src="./src/assets/Gakki.png" alt="Gakki" className=" lg:pl-0 lg:pr-0 pl-5 pr-5" /></a></div>
-                <div><a href="#"><img src="./src/assets/Gakki.png" alt="Gakki" className=" lg:pl-0 lg:pr-0 pl-5 pr-5" /></a></div>
-                <div><a href="#"><img src="./src/assets/Gakki.png" alt="Gakki" className=" lg:pl-0 lg:pr-0 pl-5 pr-5" /></a></div>
-                <div><a href="#"><img src="./src/assets/Gakki.png" alt="Gakki" className=" lg:pl-0 lg:pr-0 pl-5 pr-5" /></a></div>
+            <div className="grid grid-flow-row lg:grid-cols-6 md:grid-cols-3 grid-cols-2 pt-5">
+                <div><a href="#"><img src=".\src\assets\brand\Squier.jpg" alt="Gakki" className=" lg:pl-5 lg:pr-5 xl:pl-10 xl:pr-10 pl-5 pr-5" /></a></div>
+                <div><a href="#"><img src=".\src\assets\brand\yamaha.jpg" alt="Gakki" className=" lg:pl-5 lg:pr-5 xl:pl-10 xl:pr-10 pl-5 pr-5" /></a></div>
+                <div><a href="#"><img src=".\src\assets\brand\kurtzman.jpg" alt="Gakki" className=" lg:pl-5 lg:pr-5 xl:pl-10 xl:pr-10 pl-5 pr-5" /></a></div>
+                <div><a href="#"><img src=".\src\assets\brand\Ibanez.jpg" alt="Gakki" className=" lg:pl-5 lg:pr-5 xl:pl-10 xl:pr-10 pl-5 pr-5" /></a></div>
+                <div><a href="#"><img src=".\src\assets\brand\Fender.jpg" alt="Gakki" className=" lg:pl-5 lg:pr-5 xl:pl-10 xl:pr-10 pl-5 pr-5" /></a></div>
+                <div><a href="#"><img src=".\src\assets\brand\Epiphone.jpg" alt="Gakki" className=" lg:pl-5 lg:pr-5 xl:pl-10 xl:pr-10 pl-5 pr-5" /></a></div>
             </div>
 
             <center>
-                    <button className="w-[170px] h-[40px] my-5 border border-[#76DC8D] rounded-[7px] hover:font-semibold hover:bg-[#F3FFF1] focus:bg-[#BAFFCA] focus:font-semibold lg:hidden block">VIEW ALL</button>
-                </center>
+                <button className="w-[170px] h-[40px] my-5 border border-[#76DC8D] rounded-[7px] hover:font-semibold hover:bg-[#F3FFF1] focus:bg-[#BAFFCA] focus:font-semibold lg:hidden block">VIEW ALL</button>
+            </center>
 
             <hr className="border border-black " />
 
-            <div className="lg:flex justify-around">
+            <div className="lg:flex justify-around mt-5">
                 <center className="text-20 font-bold pt-1 lg:my-auto my-5 w-auto">Tìm theo sản phẩm</center>
                 <div className="flex lg:w-[65%]">
-                    <div><a href="#"><img src="./src/assets/Gakki.png" alt="Gakki" className=" pl-0 pr-0 sm:pl-5 sm:pr-5 " /></a></div>
-                    <div><a href="#"><img src="./src/assets/Gakki.png" alt="Gakki" className=" pl-0 pr-0 sm:pl-5 sm:pr-5 " /></a></div>
-                    <div><a href="#"><img src="./src/assets/Gakki.png" alt="Gakki" className=" pl-0 pr-0 sm:pl-5 sm:pr-5 " /></a></div>
+                    <center className="w-[33%]"><a href="#"><GiGuitarBassHead className="xl:w-[30%] lg:w-[40%] w-[50%] h-auto"></GiGuitarBassHead></a>
+                        <p className="sm:text-16 text-14 ">GUITAR & BASS</p></center>
+                    <center className="w-[33%]"><a href="#"><CgPiano className="xl:w-[30%] lg:w-[40%] w-[50%] h-auto"></CgPiano></a>
+                        <p className="sm:text-16 text-14 ">KEYBOARD</p></center>
+                    <center className="w-[33%]"><a href="#"><GiJackPlug className="xl:w-[30%] lg:w-[40%] w-[50%] h-auto"></GiJackPlug></a>
+                        <p className="sm:text-16 text-14 ">PHỤ KIỆN</p></center>
                 </div>
             </div>
         </main>
