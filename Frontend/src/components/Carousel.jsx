ec35ca32ac2ce56128af46eb605a51da7ca583cd
Brian09102003 <vonhan09102003@gmail.com>
2024-01-06 17:30:14 +0700

    Blog + Carousel

--- Frontend/src/components/Carousel.jsx
+++ Frontend/src/components/Carousel.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import { useState } from "react";
-import { PiCaretLeftBold } from "react-icons/pi";
-import { PiCaretRightBold } from "react-icons/pi";
-
+import React, { useState, useEffect } from "react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
 import Banner01 from "../assets/banners/Banner_01.png";
 import Banner02 from "../assets/banners/Banner_02.png";
 import Banner03 from "../assets/banners/Banner_03.png";
 import Banner04 from "../assets/banners/Banner_04.png";
-
 import Banner from "./Banner";
 
 function Carousel() {
-    const images = [
-        Banner01,
-        Banner02,
-        Banner03,
-        Banner04
-    ];
+  const images = [Banner01, Banner02, Banner03, Banner04];
+  const [activeIndex, setActiveIndex] = useState(0);
 
-    const [activeIndex, setActiveIndex] = useState(0);
+  const nextSlide = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
 
-    const nextSlide = () => {
-        setActiveIndex((prevIndex) => (prevIndex + 1)>=images.length ? 0 : prevIndex + 1);
-      };
-    
-      const prevSlide = () => {
-        setActiveIndex((prevIndex) => (prevIndex - 1)<0 ? images.length-1 : prevIndex - 1);
-      };    
+  const prevSlide = () => {
+    setActiveIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      nextSlide();
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [activeIndex]);
 
     return (
-    <div className="carousel flex">
+    <div className="relative carousel flex">
         <div className="w-full">
             <div className="panels flex">
                 {images.map((image, index) => (
@@ -53,18 +53,18 @@ function Carousel() {
                     />
                 ))}
             </div>
-        </div>
 
-        <div className="absolute top-0 start-0 z-30 flex items-center justify-center h-full cursor-pointer group focus:outline-none">
-            <button onClick={prevSlide} className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <PiCaretLeftBold className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"/>
+        <div className="absolute top-[50%] mx-4 start-0 z-30 flex items-center justify-center h-auto cursor-pointer group focus:outline-none">
+            <button onClick={prevSlide} className="inline-flex items-center justify-center w-10 h-10 rounded-full  text-[#1B3735] hover:text-white bg-[#B7EDAB] dark:bg-gray-800/30 group-hover:bg-[#1B3735] dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <PiCaretLeftBold className="w-4 h-4 rtl:rotate-180"/>
             </button>
         </div>
 
-        <div className="absolute top-0 end-0 z-30 flex items-center justify-center h-full cursor-pointer group focus:outline-none">
-            <button onClick={nextSlide} className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <PiCaretRightBold className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"/>
+        <div className="absolute top-[50%] mx-4 end-0 z-30 flex items-center justify-center h-auto cursor-pointer group focus:outline-none">
+            <button onClick={nextSlide} className="inline-flex items-center justify-center w-10 h-10 rounded-full  text-[#1B3735] hover:text-white bg-[#B7EDAB] dark:bg-gray-800/30 group-hover:bg-[#1B3735] dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <PiCaretRightBold className="w-4 h-4 rtl:rotate-180"/>
             </button>
+        </div>
         </div>
     </div>
   );
