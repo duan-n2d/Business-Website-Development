6c47a45ba2f857cf2b939e9338847195c1080520
Brian09102003 <vonhan09102003@gmail.com>
2024-01-06 17:44:02 +0700

    Carousel Transition
    thêm transition

--- Frontend/src/components/Carousel.jsx
+++ Frontend/src/components/Carousel.jsx
@@ -7,48 +7,67 @@ import Banner04 from "../assets/banners/Banner_04.png";
 import Banner from "./Banner";
 
 function Carousel() {
-  const images = [Banner01, Banner02, Banner03, Banner04];
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const nextSlide = () => {
-    setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const prevSlide = () => {
-    setActiveIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-  };
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      nextSlide();
-    }, 3000);
-
-    return () => {
-      clearInterval(intervalId);
+    const images = [Banner01, Banner02, Banner03, Banner04];
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [isTransitioning, setIsTransitioning] = useState(false);
+  
+    const nextSlide = () => {
+      if (!isTransitioning) {
+        setIsTransitioning(true);
+        setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }
     };
-  }, [activeIndex]);
-
+  
+    const prevSlide = () => {
+      if (!isTransitioning) {
+        setIsTransitioning(true);
+        setActiveIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+      }
+    };
+  
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        nextSlide();
+      }, 3000);
+  
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, [activeIndex, isTransitioning]);
+  
+    const handleTransitionEnd = () => {
+      setIsTransitioning(false);
+    };
+  
     return (
-    <div className="relative carousel flex">
-        <div className="w-full">
-            <div className="panels flex">
-                {images.map((image, index) => (
-                    <div key={index}
-                    className={`w-full h-auto transition-transform duration-300 transform ${
-                        index === activeIndex ? 'translate-x-0' : 'hidden'
-                    }`}>
-                    <Banner url={image} />
-                    </div>
-                ))}
-            </div>
+      <div className="relative carousel flex">
+        <div className="w-full overflow-hidden">
+          <div
+            className="panels flex"
+            style={{
+              width: `${images.length}00%`,
 
-            <div className="indicators absolute z-30 flex -translate-x-1/2  left-1/2 space-x-3 rtl:space-x-reverse">
+            }}
+            onTransitionEnd={handleTransitionEnd}
+          >
+            {images.map((image, index) => (
+              <div
+                key={index}
+                className="w-full h-auto" style={{transform: `translateX(-${activeIndex * 100}%)`,
+                transition: isTransitioning ? 'transform 0.5s ease-in-out' : 'none',}}
+              >
+                <Banner url={image} />
+              </div>
+            ))}
+          </div>
+
+            <div className="indicators absolute z-30 flex -translate-x-1/2  left-1/2 space-x-4 rtl:space-x-reverse py-3">
                 {images.map((image, index) => (
                     <button
                     key={index}
                     onClick={() => setActiveIndex(index)}
-                    className={`h-2 w-2 rounded-full transition duration-300 ${
-                        index === activeIndex ? 'bg-white' : 'bg-gray-400'
+                    className={` rounded-full transition duration-300 my-auto ${
+                        index === activeIndex ? 'bg-[#1B3735] w-4 h-4' : 'bg-gray-400 h-3 w-3'
                     }`}
                     />
                 ))}
